fix broadcast

demo.py
@@ -4,6 +4,7 @@ import os
 
 from matching2d import ShapeDetector
 from shape_info_produce import ShapeInfoProducer
+import argparse
 
 
 def angle_test(mode="test", use_rot=True, img_pth="./data"):
@@ -16,7 +17,7 @@ def angle_test(mode="test", use_rot=True, img_pth="./data"):
         print(mask.shape, img.shape)
 
         # padding to avoid rotating out
-        PADDING = 99
+        PADDING = 29
         padded_img = np.pad(
             img, ((PADDING, PADDING+1), (PADDING, PADDING+1), (0, 0)), 'constant', constant_values=0)
         padded_mask = np.pad(
@@ -69,14 +70,14 @@ def angle_test(mode="test", use_rot=True, img_pth="./data"):
         shape_info = ShapeInfoProducer(src=None)
         infos = shape_info.load_infos(info_pth="./data/test_info.yaml")
 
-        PADDING = 250
+        PADDING = 50
         padded_img = np.pad(
             img, ((PADDING, PADDING+1), (PADDING, PADDING+1), (0, 0)), 'constant', constant_values=0)
         STRIDE = 16
         n, m = padded_img.shape[0] // STRIDE, padded_img.shape[1] // STRIDE
         match_img = padded_img[:n*16, :m*16]
 
-        matches = shape_det.match(match_img, 80, ids)
+        matches = shape_det.match(match_img, 90, ids)
         if len(matches) == 0:
             print("not match anything")
             exit(0)
@@ -107,9 +108,9 @@ def angle_test(mode="test", use_rot=True, img_pth="./data"):
                 cv2.circle(match_img, (int(f.y+match.x),
                                        int(f.x+match.y)), 3, (0, 255, 0), -1)
 
-            cv2.putText(match_img, "{}".format(match.similarity),
+            cv2.putText(match_img, "{:.2f}".format(match.similarity),
                         (int(match.x+r_scaled-10), int(match.y-3)),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255))
 
         cv2.imshow("match img", match_img)
         cv2.imwrite("./data/result.jpg", match_img)
@@ -118,4 +119,13 @@ def angle_test(mode="test", use_rot=True, img_pth="./data"):
 
 
 if __name__ == "__main__":
-    angle_test("test", True, "./data/case2/test1.bmp")
+    parser = argparse.ArgumentParser(description='Shape besed matching implemented by python.')
+    parser.add_argument('--mode', type=str, 
+                        help='an integer for the accumulator')
+    parser.add_argument('--rot', type=bool, 
+                        help='sum the integers (default: find the max)')
+    parser.add_argument('--img-pth', type=str, 
+                        help='sum the integers (default: find the max)')
+    args = parser.parse_args()
+    
+    angle_test(args.mode, args.rot, args.img_pth)

feature.py
@@ -3,34 +3,37 @@ import cv2
 
 NEIGHBOR_THRESHOLD = 5
 
+
 class Point(object):
     # x, y
     def __init__(self, x, y):
         self.x, self.y = x, y
-        
+
+
 class Feature(object):
     # x, y, label, theta
     def __init__(self, x=0, y=0, label=0, theta=0.0):
         self.x, self.y, self.label, self.theta = x, y, label, theta
-        
-    def read(self, feature:list()):
+
+    def read(self, feature: list()):
         self.x, self.y, self.label = feature
-    
+
     def write(self, save_pth):
         pass
-    
+
+
 class Template(object):
-    
-    def __init__(self, 
-                 width=None, height=None, 
-                 tl_x=None, tl_y=None, 
+
+    def __init__(self,
+                 width=None, height=None,
+                 tl_x=None, tl_y=None,
                  pyramid_level=None, features=list()):
         self.width, self.height = width, height
         self.tl_x, self.tl_y = tl_x, tl_y
         self.pyramid_level = pyramid_level
         self.features = features
-        
-    def read(self, templ:dict()):
+
+    def read(self, templ: dict()):
         self.width = templ['width']
         self.height = templ['height']
         self.tl_x = templ['tl_x']
@@ -42,9 +45,9 @@ class Template(object):
             feat.read(f)
             features.append(feat)
         self.features = features
-    
+
     def write(self, save_pth):
-        pass  
+        pass
 
 
 class Candidate(object):
@@ -52,6 +55,7 @@ class Candidate(object):
     def __init__(self, x, y, label, score):
         self.x, self.y, self.label, self.score = x, y, label, score
         self.f = Feature(x=x, y=y, label=label)
+
 
 def hysteresisGradient(magnitude, angle, threshold):
     quantized_angle = np.zeros_like(angle, np.uint8)
@@ -66,12 +70,10 @@ def hysteresisGradient(magnitude, angle, threshold):
     quantized_unfiltered[-1, :] = 0
     quantized_unfiltered[:, 0] = 0
     quantized_unfiltered[:, -1] = 0
-    
+
     # Mask 16 buckets into 8 quantized orientations
     quantized_unfiltered[1:-1, 1:-1] %= 7
-    # cv2.imshow("quantized_unfiltered", quantized_unfiltered)
-    # cv2.waitKey(0)
-    # print(magnitude.shape, quantized_unfiltered.shape)
+
     # Filter the raw quantized image. Only accept pixels where the magnitude is above some
     # threshold, and there is local agreement on the quantization.
     for j in range(1, angle.shape[1]-1):
@@ -82,7 +84,7 @@ def hysteresisGradient(magnitude, angle, threshold):
                 patch3x3 = quantized_unfiltered[i-1:i+2, j-1:j+2].flatten()
                 for index in patch3x3:
                     histogram[index] += 1
-                
+
                 max_votes = max(histogram)
                 index = np.argmax(histogram)
                 # Only accept the quantization if majority of pixels in the patch agree
@@ -91,12 +93,8 @@ def hysteresisGradient(magnitude, angle, threshold):
     return quantized_angle
 
 
-# def select_scattered_features(candidates, num_feat, distance):
-    
-    
-    
 class ColorGradientPyramid(object):
-    
+
     def __init__(self, src, mask, weak_thresh, num_feat, strong_thresh):
         self.src = src
         self.mask = mask
@@ -104,63 +102,58 @@ class ColorGradientPyramid(object):
         self.weak_thresh = weak_thresh
         self.num_feat = num_feat
         self.strong_thresh = strong_thresh
-        
+
         self.angle = None
         self.magnitude = None
         self.angle_ori = None
-        
+
         self._update()
-        
+
     def _update(self):
-        # Compute horizontal and vertical image derivatives on 
+        # Compute horizontal and vertical image derivatives on
         # all color channels separately
         KERNEL_SIZE = (7, 7)
         smoothed = cv2.GaussianBlur(
             self.src, KERNEL_SIZE, 0, sigmaY=0, borderType=cv2.BORDER_REPLICATE)
         if len(self.src.shape) == 2 or \
-            (len(self.src.shape) == 3 and self.src.shape[2] == 1):
+                (len(self.src.shape) == 3 and self.src.shape[2] == 1):
             sobel_dx = cv2.Sobel(
                 smoothed, cv2.CV_32F, 1, 0, None, 3, 1.0, 0.0, borderType=cv2.BORDER_REPLICATE)
             sobel_dy = cv2.Sobel(
                 smoothed, cv2.CV_32F, 0, 1, None, 3, 1.0, 0.0, borderType=cv2.BORDER_REPLICATE)
             magnitude = sobel_dx*sobel_dx + sobel_dy*sobel_dy
-            
+
         else:
-            # magnitude = np.zeros_like(self.src, np.float32)
-            # src_shape = self.src.shape
-            # sobel_dx = np.zeros((src_shape[1], src_shape[0]), np.float32)
-            # sobel_dy = np.zeros((src_shape[1], src_shape[0]), np.float32)
-            
             sobel_3dx = cv2.Sobel(
                 smoothed, cv2.CV_16S, 1, 0, None, 3, 1.0, 0.0, borderType=cv2.BORDER_REPLICATE)
             sobel_3dy = cv2.Sobel(
                 smoothed, cv2.CV_16S, 0, 1, None, 3, 1.0, 0.0, borderType=cv2.BORDER_REPLICATE)
-            
+
             # Use the gradient orientation of the channel whose magnitude is largest
+            sobel_3dx = sobel_3dx.astype(np.float32)  # data type must to be converted
+            sobel_3dy = sobel_3dy.astype(np.float32)
             sobel_3d = sobel_3dx*sobel_3dx+sobel_3dy*sobel_3dy
             magnitude = np.max(sobel_3d, axis=2)
             h, w = magnitude.shape[:2]
             inds = np.argmax(sobel_3d, axis=2)
-            xx, yy = np.meshgrid(np.arange(w), np.arange(h))
-            sobel_dx = sobel_3dx[yy, xx, inds]
+            xx, yy = np.meshgrid(np.arange(h), np.arange(w))
+            sobel_dx = sobel_3dx[xx.T, yy.T, inds]
             # sobel_dx_vis = cv2.convertScaleAbs(sobel_dx)
-            sobel_dy = sobel_3dy[yy, xx, inds]
+            sobel_dy = sobel_3dy[xx.T, yy.T, inds]
 
-        sobel_ag = cv2.phase(sobel_dx.astype(np.float32), sobel_dy.astype(np.float32), angleInDegrees=True)
-        self.angle = hysteresisGradient(magnitude, sobel_ag, self.weak_thresh**2)
+        sobel_ag = cv2.phase(sobel_dx.astype(np.float32),
+                             sobel_dy.astype(np.float32), angleInDegrees=True)
+        self.angle = hysteresisGradient(
+            magnitude.astype(np.float32), sobel_ag, self.weak_thresh**2)
         self.angle_ori = sobel_ag
         self.magnitude = magnitude.astype(np.float32)
-        # cv2.imshow("angle", self.angle)
-        # cv2.waitKey(0)
-        # cv2.imshow("sobel ag", sobel_ag)
-        # cv2.waitKey(0)
-            
+
     def _select_scattered_features(self, candidates, num_feat, distance):
         distance_sq = distance**2
-    
+
         features = []
         i, first_select = 0, True
-        
+
         while True:
             c = candidates[i]
             keep = True
@@ -190,41 +183,38 @@ class ColorGradientPyramid(object):
                 if num_ok or distance < 3:
                     break
         return features
-    
+
     def quantize(self):
-        # dst = np.zeros_like(self.angle, np.uint8)
         return cv2.copyTo(self.angle, self.mask)
-    
+
     def extract_template(self) -> Template:
         if self.mask is None:
             return None
-        
+
         candidates = []
         mask = self.mask.astype(np.uint8)*255
         local_mask = cv2.erode(
             mask, kernel=None, anchor=(-1, -1), iterations=1, borderType=cv2.BORDER_REPLICATE)
         threshold_sq = self.strong_thresh**2
-        nms_kernel_size = 5 
+        nms_kernel_size = 5
         nms_ksize = nms_kernel_size // 2
         magnitude_valid = np.ones_like(self.magnitude, np.uint8)
-        # print("max mask: ", np.max(mask))
-        # cv2.imshow("mask", mask)
-        # cv2.waitKey(0)
-        # magnitude_valid = np.maximum(self.magnitude, threshold_sq)
+
         h, w = self.magnitude.shape[:2]
         for i in range(nms_ksize, h-nms_ksize, 1):
             for j in range(nms_ksize, w-nms_ksize, 1):
                 if local_mask[i, j] and magnitude_valid[i, j]:
-                    score = self.magnitude[i-nms_ksize:i+nms_ksize, j-nms_ksize:j+nms_ksize].max()
+                    score = self.magnitude[
+                        i-nms_ksize:i + nms_ksize, j-nms_ksize:j+nms_ksize].max()
                     if score == self.magnitude[i, j]:
-                        magnitude_valid[i-nms_ksize:i+nms_ksize, j-nms_ksize:j+nms_ksize] = 0
-                        magnitude_valid[i, j] = 1   
-                        # print(score, threshold_sq, self.angle[i, j])
-                        if score > threshold_sq and self.angle[i, j]>0:
-                            label = -1 if self.angle[i, j] > 128 else int(np.log2(self.angle[i, j]))
+                        magnitude_valid[
+                            i-nms_ksize:i + nms_ksize, j-nms_ksize:j+nms_ksize] = 0
+                        magnitude_valid[i, j] = 1
+                        if score > threshold_sq and self.angle[i, j] > 0:
+                            label = -1 if self.angle[i, j] > 128 else int(
+                                    np.log2(self.angle[i, j]))
                             candidate = Candidate(i, j, label, score)
                             candidate.f.theta = self.angle_ori[i, j]
-                            # print(i, j, label, score, candidate.f.theta)
                             candidates.append(candidate)
         # We require a certain number of features
         num_cand = len(candidates)
@@ -233,62 +223,65 @@ class ColorGradientPyramid(object):
                 print("too few features, abort")
                 return None
             print("have no enough features, exaustive mode")
-            
+
         candidates = sorted(candidates, key=lambda x: x.score)
-        
-        distance = num_cand /self.num_feat + 1
+
+        distance = num_cand / self.num_feat + 1
         templ = Template(-1, -1, None)
-        templ.features = self._select_scattered_features(candidates, self.num_feat, distance)
+        templ.features = self._select_scattered_features(
+            candidates, self.num_feat, distance)
         templ.pyramid_level = self.pyramid_level
         return templ
 
     def pyramid_down(self):
         # Some parameters need to be adjusted
-        self.num_feat /= 2 # @todo Why not 4?
+        self.num_feat /= 2  # @todo Why not 4?
         self.pyramid_level += 1
-        
+
         # Downsample the current inputs
         h, w = self.src.shape[:2]
         dsize = (w//2, h//2)
-        # print(self.src.shape, dsize)
         self.src = cv2.pyrDown(self.src, dstsize=dsize)
-        
+
         if self.mask is not None:
             self.mask = cv2.resize(
                 self.mask.astype(np.uint8), dsize=dsize, fx=0.0, fy=0.0, interpolation=cv2.INTER_NEAREST)
 
         self._update()
 
+
 class ColorGradient:
     # weak_threshold, num_features, strong_threshold
-    
+
     def __init__(self, weak_thresh=30.0, num_feat=63, strong_thresh=60.0):
         self.weak_threshold = weak_thresh
         self.num_features = num_feat
         self.strong_threshold = strong_thresh
-        
+
     def read(self, f_pth):
         pass
-    
+
     def write(self, save_pth):
-        pass  
-    
+        pass
+
     def process(self, src, mask) -> ColorGradientPyramid:
-        return ColorGradientPyramid(src, mask, self.weak_threshold, self.num_features, self.strong_threshold) 
+        return ColorGradientPyramid(src, mask, self.weak_threshold, self.num_features, self.strong_threshold)
+
 
 class Match(object):
-    
+
     def __init__(self, x, y, similarity, class_id, template_id):
         self.x, self.y = x, y
         self.similarity = similarity
         self.class_id = class_id
         self.template_id = template_id
-        
+
     def sort_matches(self, match):
         pass
-    
+
     def is_equal(self):
         pass
+
 
 class Info:
     # angle, scale

matching2d.py
@@ -281,8 +281,6 @@ class ShapeDetector(object):
             # @todo Factor this out into separate function
             lowest_lm = lm_pyramid[-1]
             # Compute similarity maps for each ColorGradient at lowest pyramid level
-            # candidates = []
-            # lowest_start = len(tp) - 1
             lowest_T = self.T_at_level[-1]
             num_features = 0
             templ = tp[-1]
@@ -330,8 +328,8 @@ class ShapeDetector(object):
                     match2.similarity = np.max(scores)
                     # r = best_ind % scores.shape[1]
                     # c = best_ind // scores.shape[1]
-                    match2.x = (x / T - 8 + c) * T + offset
-                    match2.y = (y / T - 8 + r) * T + offset
+                    match2.x = (x / T - 8 + r) * T + offset
+                    match2.y = (y / T - 8 + c) * T + offset
 
                 new_candidates = list(
                     filter(lambda x: x.similarity >= threshold, candidates))
@@ -381,8 +379,9 @@ class ShapeDetector(object):
                                              threshold, class_id, self.temp_class_map[class_id])
         print("length of matches: ", len(matches))
         # Sort matches by similarity, and prune any duplicates introduced by pyramid refinement
-        matches_sorted = sorted(matches, key=lambda x: (
-            x.similarity, x.template_id), reverse=True)
+        matches_sorted = sorted(matches, 
+                                key=lambda x: (x.similarity, x.template_id), 
+                                reverse=True)
         matches_filter = list()
         for ms in matches_sorted:
             if len(matches_filter) <= 0: